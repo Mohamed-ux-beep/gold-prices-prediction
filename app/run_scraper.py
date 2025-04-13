c5aead520d17500e173d275b521a93314bddfe94
mohamed <mohamed.abokahf@student.hs-anhalt.de>
2025-04-13 12:15:46 +0200

    added run_scraper script

---
+++ app/run_scraper.py
@@ -0,0 +1,9 @@
+from app.utils import fetch_gold_price, save_to_csv
+from datetime import datetime
+
+price = fetch_gold_price()
+if price:
+    save_to_csv(datetime.utcnow().isoformat(), price)
+else:
+    print('❌ No price fetched')
+
